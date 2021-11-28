51b95bbf37d3f71e29acb24f50e54428241e802b
okitauehara <marcosokita@live.com>
2021-11-28 00:18:34 -0300

    Post transaction tests implemented

--- tests/transaction.test.js
+++ tests/transaction.test.js
@@ -2,7 +2,8 @@
 import supertest from 'supertest';
 import app from '../src/app.js';
 import connection from '../src/database/database.js';
-import * as U from './factories/user.factory';
+import * as U from './factories/user.factory.js';
+import * as T from './factories/transaction.factory.js';
 
 afterAll(async () => {
   await U.deleteUserSessions();
@@ -30,5 +31,27 @@ describe('GET /transactions', () => {
   test('should return status 404 if the request does not return any results', async () => {
     const result = await supertest(app).get('/transactions').set('Authorization', `Bearer ${U.fakeSession.token}123`);
     expect(result.status).toEqual(404);
+  });
+});
+
+describe('POST /transactions', () => {
+  test('should return status 201 if the register was successfull created', async () => {
+    const result = await supertest(app).post('/transactions').send(T.fakeTransaction).set('Authorization', `Bearer ${U.fakeSession.token}`);
+    expect(result.status).toEqual(201);
+  });
+
+  test('should return status 401 if the request was missing token', async () => {
+    const result = await supertest(app).post('/transactions').send(T.fakeTransaction);
+    expect(result.status).toEqual(401);
+  });
+
+  test('should return status 404 if the request does not return any results', async () => {
+    const result = await supertest(app).post('/transactions').set('Authorization', `Bearer ${U.fakeSession.token}123`);
+    expect(result.status).toEqual(404);
+  });
+
+  test('should return status 400 if the request did not passed the joi validation', async () => {
+    const result = await supertest(app).post('/transactions').send(T.invalidFakeTransaction).set('Authorization', `Bearer ${U.fakeSession.token}`);
+    expect(result.status).toEqual(400);
   });
 });
